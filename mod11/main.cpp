d7128b5a7925829d62eab3cd112871cd6648d04d
Dheeraj Kumar <68736407+code-official@users.noreply.github.com>
2023-04-03 11:58:33 +0530

    Add files via upload

---
+++ mod11/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include "test.h"
+int main(int argc, char *argv[])
+{
+    //timer vid7
+    QCoreApplication a(argc, argv);
+
+    Test test;
+
+    return a.exec();
+}
